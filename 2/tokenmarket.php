5b3ee967da5a1c073b781c324f59b297a2f91d1e
932563595 <932563595@qq.com>
2018-03-26 09:55:27 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/tokenmarket.php

---
+++ 2/tokenmarket.php
@@ -0,0 +1,51 @@
+<?php
+include('bootstraps.php');
+
+$url = 'https://tokenmarket.net/blockchain/all-assets';
+$content = file_get_contents($url);
+$counts=explode("<small>Showing <strong>",$contents)[1];
+$counts=explode("</strong> assets</small>",$counts)[0];
+echo $counts;die;
+$str = getSonString($content, '<div class="table-responsive compact-name-column">', '<div class="pull-right');
+$str = getSonString($str, '<tbody>', '</tbody>');
+// echo $str;
+
+$rowList = explode('</tr>', $str);
+$resList = array();
+foreach ($rowList as $row) {
+	$colList = explode('</td>', $row);
+	$row = array();
+	foreach ($colList as $col) {
+		// var_dump($col);
+		$colvalue = trim(getSonString2($col, '>', '<'));
+		// echo $colvalue.'<br>';
+		$row[] = $colvalue;
+		// break;
+	}
+	array_pop($row);
+	$resList[] = $row;
+	// break;
+}
+array_pop($resList);
+
+// foreach ($resList as $res) {
+// 	$symbol = $res[2];
+// 	$name = $res[1];
+// 	$sql = "insert into `basic_token_list` (`symbol`,`name`) values ('".$symbol."','".$name."');";
+// 	MySQLRunSQL($sql);
+// 	echo $symbol.',';
+// }
+// echo json_encode($resList);
+
+$sql = "select * from `basic_token_list`";
+$table = MySQLGetData($sql);
+if(count($resList) > count($table)) {
+	for($i = count($table); $i < count($resList); $i++) {
+		$res = $resList[$i];
+		$symbol = $res[2];
+		$name = $res[1];
+		$sql = "insert into `basic_token_list` (`symbol`,`name`) values ('".$symbol."','".$name."');";
+		MySQLRunSQL($sql);
+		echo $symbol.',';
+	}
+}
