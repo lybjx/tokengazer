f447949aba1bc9c024d565d25da675e2d40fed38
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 04:54:45 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -5,5 +5,6 @@ $snoopy=new Snoopy();
 
 $url = 'https://coinmarketcap.com/';
 $content = file_get_contents_https($url);
+$content=getSonString($content,"class=\"col-xs-12\"","</div>");
 $url1 = getSonStrings($content, '<a class="currency-name-container" href="','</a>');
 print_r($url1);
