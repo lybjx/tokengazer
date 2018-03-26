e98da2cde7d0266a38222b4fa94b80178c84cca9
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 07:38:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/icorating.php

--- 2/icorating.php
+++ 2/icorating.php
@@ -17,7 +17,7 @@ $str2=explode('<td>',$str1);
 $i=0;
 //print_r($str2);die;
 foreach($str2 as $k=>$v){
-    if($k==0||$k%2==0){return;
+    if($k==0||$k%2==0){continue;
 $url=getSonString($str2[$k],"<tr data-href='","'>",$str2[$k]);
         $con1=file_get_contents_https($url);
         $str3=explode("github.com/",$con1)[1];
