abe8eb7130436de9dbd6064cd097fe39d486a617
932563595 <932563595@qq.com>
2018-03-26 05:51:25 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -16,17 +16,14 @@ foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $githuburl[$k]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
 }
-$url = 'https://coinmarketcap.com/1';
-$content = file_get_contents_https($url);
-$content=getSonString($content,"<tbody>","</tbody>");
-$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
 $i=1;
 foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
-    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $arr[$i]['name']=explode("/",$url1[$k])[2];
+    $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
     $i++;
 }
-$url = 'https://coinmarketcap.com/3';
+/*$url = 'https://coinmarketcap.com/3';
 $content = file_get_contents_https($url);
 $content=getSonString($content,"<tbody>","</tbody>");
 $url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
@@ -52,5 +49,6 @@ foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
     $i++;
-}
+}*/
 print_r($githuburl);
+foreach 
