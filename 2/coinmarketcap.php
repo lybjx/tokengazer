4d98f10cef706a61d424cb38e52b82451418d099
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 06:02:32 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -12,12 +12,19 @@ $content = file_get_contents_https($url);
 $content=getSonString($content,"<tbody>","</tbody>");
 $url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
 $githuburl=array();
+$ret = $kv->init("xowlw2kmk2");
 foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $arr[$i]['name']=explode("/",$url1[$k])[2];
     $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $ret = $kv->set('products:'.$, $url1[$kk]);
     $i++;
 }
+$kv = new SaeKV();
+
+// 初始化SaeKV对象
+//访问授权应用的数据
+
 /*$url = 'https://coinmarketcap.com/3';
 $content = file_get_contents_https($url);
 $content=getSonString($content,"<tbody>","</tbody>");
