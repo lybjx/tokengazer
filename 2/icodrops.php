ace77fa64104c920b7af72479d25037a46230f6e
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 09:26:05 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/icodrops.php

--- 2/icodrops.php
+++ 2/icodrops.php
@@ -6,4 +6,7 @@ $content = file_get_contents_https($url);
 $str1 = trim(getSonString($content, "<h3 class=\"col-md-12 col-12 not_rated\">All</h3>", "<div class=\"tabs__content\">"));
 $str2=explode("<div class=\"ico-main-info\">",$str1);
 unset($str2[0]);
+foreach($str2 as $k=>$v){
+	echo $url=explode("\" rel=\"bookmark\">",explode("<h3><a href=\"",$str2[$k])[1])[0];
+}
 print_r($str2);
