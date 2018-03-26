fb1ebd1b1111b3b684b630f0460059eca2b53f91
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 06:31:29 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -25,7 +25,7 @@ foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $arr[$i]['name']=explode("/",$url1[$k])[2];
     $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
-    $kv->add('products:'.$i, $arr[$i]);
+    $kv->add('products:'.$i, json_encode($arr[$i],true));
     
     echo $kv->get('products:'.$i);
     $i++;
