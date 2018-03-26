b0b5a9b7e464e82cb5dbbfa800ea846fde3a78a0
932563595 <932563595@qq.com>
2018-03-26 05:37:52 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -2,13 +2,55 @@
 include('bootstraps.php');
 include('snoopy.php');
 $snoopy=new Snoopy();
-
-$url = 'https://coinmarketcap.com/';
+if(isset($_GET['p'])){
+$p=$_GET['p'];
+}
+else{
+$p=1;
+}
+$url = 'https://coinmarketcap.com/'.$p;
 $content = file_get_contents_https($url);
 $content=getSonString($content,"<tbody>","</tbody>");
 $url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
 foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $githuburl[$k]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+}
+$url = 'https://coinmarketcap.com/1';
+$content = file_get_contents_https($url);
+$content=getSonString($content,"<tbody>","</tbody>");
+$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
+$i=1;
+foreach($url1 as $k=>$v){
+$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
+    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $i++;
+}
+$url = 'https://coinmarketcap.com/3';
+$content = file_get_contents_https($url);
+$content=getSonString($content,"<tbody>","</tbody>");
+$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
+foreach($url1 as $k=>$v){
+$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
+    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $i++;
+}
+$url = 'https://coinmarketcap.com/4';
+$content = file_get_contents_https($url);
+$content=getSonString($content,"<tbody>","</tbody>");
+$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
+foreach($url1 as $k=>$v){
+$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
+    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $i++;
+}
+$url = 'https://coinmarketcap.com/5';
+$content = file_get_contents_https($url);
+$content=getSonString($content,"<tbody>","</tbody>");
+$url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
+foreach($url1 as $k=>$v){
+$contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
+    $githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
+    $i++;
 }
 print_r($githuburl);
