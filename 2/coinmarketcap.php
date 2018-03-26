5a33d388ec6f2cf4aeda8b5e2b6d3171fe94d7f9
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-26 06:20:24 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -7,6 +7,7 @@ $p=$_GET['p'];
 else{
 $p='';
 }
+
 $url = 'https://coinmarketcap.com/'.$p;
 $content = file_get_contents_https($url);
 $content=getSonString($content,"<tbody>","</tbody>");
@@ -14,6 +15,15 @@ $url1 = getSonStrings($content, '<span class="currency-symbol"><a href="','">');
 $githuburl=array();
 $kv = new SaeKV();
 $ret = $kv->init("xowlw2kmk2");
+$ret = $kv->pkrget('', 100);
+while (true) {
+   var_dump($ret);
+   end($ret);
+   $start_key = key($ret);
+   $i = count($ret);
+   if ($i < 100) break;
+   $ret = $kv->pkrget('', 100, $start_key);
+}die;
 foreach($url1 as $k=>$v){
 $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $arr[$i]['name']=explode("/",$url1[$k])[2];
