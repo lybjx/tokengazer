888c8bdab6d28fd9beea398f62a9776381bac702
932563595 <932563595@qq.com>
2018-03-26 07:56:06 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/icorating.php

--- 2/icorating.php
+++ 2/icorating.php
@@ -18,6 +18,7 @@ $i=0;
 //print_r($str2);die;
 foreach($str2 as $k=>$v){
     if($k==0||$k%2==0){
+        echo $str2[$k];
     echo $name=getSonString($str2[$k],"\" />","</td>");
     }
     if($k==0||$k%2==0){/*continue;
