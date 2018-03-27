f1abc2664afe69fee7b4a1faa75ef5a2007ef9e5
932563595 <932563595@qq.com>
2018-03-27 02:18:00 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/putfile.php

--- 2/putfile.php
+++ 2/putfile.php
@@ -1,4 +1,4 @@
 <?php
-new sinacloud\sae\Storage();
-
+$storage=new sinacloud\sae\Storage();
+$storage->__construct('xowlw2kmk2', 'xwz3045l2wmzxz1zij4j50wm5xx104i42wkxz5l2',DEFAULT_USE_SSL,DEFAULT_STORAGE_ENDPOINT)
 ?>
