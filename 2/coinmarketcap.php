13d726370c6aaf1b976fda12ebf8d67440ab3ef9
932563595@qq.com <932563595@qq.com@5aeb8729-67c5-4a57-aa45-1e466164e9e9>
2018-03-27 10:14:15 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/coinmarketcap.php

--- 2/coinmarketcap.php
+++ 2/coinmarketcap.php
@@ -23,11 +23,10 @@ $contents1=file_get_contents_https("https://coinmarketcap.com".$url1[$k]);
     $arr[$i]['githuburl']=$githuburl[$i]=getSonString($contents1,'<span class="glyphicon glyphicon-hdd text-gray" title="Source Code"></span> <a href="','"');
     $kv->add('products:'.$i, json_encode($arr[$i],true));
     
-    echo $kv->get('products:'.$i);
+    $kv->get('products:'.$i);
     $i++;
 }
 
-print_r($arr);
 // 初始化SaeKV对象
 //访问授权应用的数据
 
