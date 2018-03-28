3437608341796938aee72f8e157a8239486a739d
932563595 <932563595@qq.com>
2018-03-28 01:32:50 +0000

    Autoversioning commit:  a non-deltaV client made a change to /2/icorating.php

--- 2/icorating.php
+++ 2/icorating.php
@@ -30,12 +30,12 @@ foreach($str2 as $k=>$v){
         }
         $url=getSonString($str2[$k],"<tr data-href='","'>",$str2[$k]);
         $con1=file_get_contents_https($url);
-        $str3=explode("github.com/",$con1)[1];
+        /*$str3=explode("github.com/",$con1)[1];
         $str4=explode("\"",$str3)[0];
         $arr[$i]['githuburl']="https://github.com/".$str4;
         $ret = $kv->delete('products:'.$i,);
         $kv->add('products:'.$i, json_encode($arr[$i],true));
-    
+    */
     //echo $kv->get('products:'.$i);
     
     } 
